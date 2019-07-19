942de2ffcd1c437ad4008047b7b033c09c0c7d4f
vithyaCodegama <vithya@codegama.com>
2019-07-19 10:25:11 +0530

    Basic product setup + Migrations created

---
+++ app/Constants/constants.php
@@ -0,0 +1,17 @@
+<?php 
+
+if(!defined('NO')) define('NO', 0);
+if(!defined('YES')) define('YES', 1);
+
+if(!defined('PAID')) define('PAID',1);
+if(!defined('UNPAID')) define('UNPAID', 0);
+
+if(!defined('APPROVED')) define('APPROVED', 1);
+if(!defined('DECLINED')) define('DECLINED', 0);
+
+if(!defined('DEFAULT_TRUE')) define('DEFAULT_TRUE', true);
+if(!defined('DEFAULT_FALSE')) define('DEFAULT_FALSE', false);
+
+if(!defined('ADMIN')) define('ADMIN', 'admin');
+if(!defined('USER')) define('USER', 'user');
+if(!defined('PROVIDER')) define('PROVIDER', 'provider');
