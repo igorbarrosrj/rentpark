85bcc87b58a6ac85ad79663e4891dae366a74779
naveen <naveensnlbe@gmail.com>
2019-08-08 19:30:39 +0530

    Database Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
+        
+        //$this->call(UserSeeder::class);
 
-        $this->call(AdminSeeder::class);
+        //$this->call(AdminSeeder::class);
 
         $this->call(SettingSeeder::class);
       
